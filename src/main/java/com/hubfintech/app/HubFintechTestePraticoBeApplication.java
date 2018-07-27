96af8af805c7de0ab50bf3b4d587d62b43e5f407
Silvio Angelo <silvioangels@gmail.com>
2018-07-27 12:03:27 -0300

    Versão Inicial

---
+++ src/main/java/com/hubfintech/app/HubFintechTestePraticoBeApplication.java
@@ -0,0 +1,12 @@
+package com.hubfintech.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubFintechTestePraticoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubFintechTestePraticoBeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/hubfintech/app/HubFintechTestePraticoBeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hubfintech.app;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HubFintechTestePraticoBeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
